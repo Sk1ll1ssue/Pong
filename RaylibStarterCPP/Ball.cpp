5d04335cfaaccad38c4b421ad997c4eb36b33384
AIE\s220465 <s220465@students.aie.edu.au>
2023-06-22 12:20:32 +0930

    Set up ball

--- RaylibStarterCPP/Ball.cpp
+++ RaylibStarterCPP/Ball.cpp
@@ -1,2 +1,15 @@
 #include "Ball.h"
 
+Ball::Ball()
+{
+	Vector2 position;
+	Vector2 speed;
+	bool active;
+	int radius;
+
+}
+
+void Ball::Draw()
+{
+	DrawCircle(position.x, position.y, radius, DARKBLUE);
+}
